7e29bf8e68630ae881c7ad31a25f22816ad92b91
Philip Andersson <philip.andersson.1991@hotmail.com>
2024-06-27 12:29:52 +0200

    temp_logo

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule CustomerAdmin.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:faker, "~> 0.18.0"}
     ]
   end
 
